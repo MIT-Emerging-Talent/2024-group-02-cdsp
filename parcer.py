1545d695753e84357a995400343a8271576290f8
Vlad421 <35481094+Vlad421@users.noreply.github.com>
2024-02-04 23:32:18 -0700

    - need som fixes

--- parcer.py
+++ parcer.py
@@ -4,8 +4,8 @@
 """
 File: parcer.py
 Author: Vlad421
-Date: 1/23/2024
-Description: collects data from layoffs.fyi
+Date: 2/4/2024
+Description: collects data from layoffs.fyi using Selenium with ChromeDriver
 Requirements: selenium, BeautifulSoup, html5lib
 
 pip install selenium, bs4, html5lib
@@ -14,7 +14,7 @@ pip install selenium, bs4, html5lib
 
 
 import time
-
+import csv
 
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
@@ -22,36 +22,32 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from bs4 import BeautifulSoup
 
+
 def update_pos(scrH, panH, curP):
-    """linear relationship
+    """Linear relationship function for step position update
     """
 
-    print(scrH)
-    print(panH)
-    print(curP)
-    x1 = 1000
-    y1= scrH-panH
-    x2= 10
+    # initial proportion
+    x1 = 900
+    y1 = scrH-panH
+    x2 = 5
     y2 = scrH
+
     # m = (y2-y1)/(x2-x1)
-    m =(y2-y1)/(x2-x1)
-    
+    m = (y2-y1)/(x2-x1)
     # y = mx+b
     # y = y1, x = x1
     b = y1-(x1*m)
-    
-    #x = (y-b)/m
-    pos = int((curP-b)/m)
-    print(pos)
 
+    # x = (y-b)/m
+    pos = int((curP-b)/m)
 
     return pos
-    
 
 
 # Define the Chrome webdriver options
 opts = ChromeOptions()
-opts.add_argument("--window-size=1000,10440")
+opts.add_argument("--window-size=3000,10440")
 opts.add_argument("--headless")
 opts.add_argument('--blink-settings=imagesEnabled=false')
 opts.add_argument("--log-level=3")
@@ -68,7 +64,6 @@ driver.get(URL)
 time.sleep(5)
 
 
-
 number_of_rows_s = driver.find_element(
     By.CSS_SELECTOR, "#view > div > div.paneContainer > div.summaryBarContainer > div.leftPaneWrapper > div > div > div > div.absolute.z1.flex-inline.border-top.border-transparent > div").text
 number_of_rows = ""
@@ -80,7 +75,7 @@ number_of_rows = int(number_of_rows)
 
 html = ""
 table = {}
-sourceFile = open('txtoutput.html', 'w', encoding="utf-8")
+#sourceFile = open('txtoutput.html', 'w', encoding="utf-8")
 slider = driver.find_element(
     By.CSS_SELECTOR, "#view > div > div.paneContainer > div.scrollOverlay.antiscroll-wrap > div.antiscroll-scrollbar.antiscroll-scrollbar-vertical.antiscroll-scrollbar-shown")
 
@@ -88,30 +83,22 @@ slider = driver.find_element(
 dist = 3
 data = driver.find_element(By.CSS_SELECTOR, "#view > div > div.paneContainer ")
 index = 0
-jsSyyle = "'3px solid red'"
 
 
 page_height = driver.get_window_size()['height']  # Get page height
 
-'''scroll_bar = driver.find_element(
-    By.XPATH, "//*[@id=\"view\"]/div/div[1]/div[1]/div[3]")
-print(scroll_bar.location[y])
-# Substracted 160 fro page height to compensate differnec between window and screen height
-ActionChains(driver).drag_and_drop_by_offset(
-    scroll_bar, 0, scroll_bar.location.values).perform()
 
-'''
-scroll_pane_height = driver.find_element(By.CSS_SELECTOR, "#view > div > div.paneContainer > div.scrollOverlay.antiscroll-wrap").size['height']
+scroll_pane_height = driver.find_element(
+    By.CSS_SELECTOR, "#view > div > div.paneContainer > div.scrollOverlay.antiscroll-wrap").size['height']
 
 
 prev_size = 0
 new_pos = 0
-#while False:
+# while False:
 
 prev_loc = 0
-#for i in  range (1):
+# for i in  range (1):
 while len(table) != number_of_rows:
-    driver.execute_script("arguments[0].style.border=" + jsSyyle, slider)
 
     # If this line causes an error, run 'pip install html5lib' or install html5lib
     soup = BeautifulSoup(data.get_attribute('innerHTML'), 'html5lib')
@@ -124,40 +111,34 @@ while len(table) != number_of_rows:
         if index not in table:
             table[index] = []
         text = row.text
+        if text == None:
+            text=''
         info = table.get(index)
         if text not in info:
             table.get(index).append(row.text)
         # info.append(row.text)
-    print(len(table))
-    """
-    if prev_size == len(table) and  slider.location['y'] ==  prev_loc:
-        ActionChains(driver).move_to_element(slider).click_and_hold(slider).move_by_offset(0, -400).release().perform()
-        print("performed")
-        time.sleep(0.5)
-    """
+    print("Aquired - "+str(len(table))+" - lines")
 
-    # ActionChains(driver).move_to_element(slider).click_and_hold(slider).move_by_offset(0, dist).release().perform()
-    
-    
     new_pos = update_pos(page_height, scroll_pane_height, slider.location['y'])
-    
-    #ActionChains(driver).drag_and_drop( slider, 0, new_pos).click().perform()
-    ActionChains(driver).move_to_element(slider).click_and_hold(slider).move_by_offset(0, new_pos).release().perform()
+
+    # ActionChains(driver).drag_and_drop( slider, 0, new_pos).click().perform()
+    ActionChains(driver).move_to_element(slider).click_and_hold(
+        slider).move_by_offset(0, new_pos).release().perform()
     time.sleep(2.5)
-    
+
     prev_loc = slider.location['y']
     prev_size = len(table)
-    
-    
 
-print("done")
 
 time.sleep(2)
 
+out = []
 for i in table:
+    row = [i]+table[i]
 
-    print(i, end=" - ")
-    print("\t"+str(table[i]))
+
+    out.append(row)
+
 
 prev = -1
 is_done = False
@@ -172,6 +153,12 @@ for i in table:
 if is_done:
     print("everithing correct ")
 
-# soup = BeautifulSoup(table.get_attribute('innerHTML'), 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
 
-# print(soup,file=sourceFile)
+for o in out:
+    print(o)
+
+file = ".layoffs_" +str(time.time())+".csv"
+with open(file, 'w', newline='') as output:
+     wr = csv.writer(output,delimiter=';', quoting=csv.QUOTE_MINIMAL)
+     wr.writerow(["id","Company","Location","Industry", "# Laid Off", "%", "Date", "Source", "Country", "Crunchbase","Time added"])
+     wr.writerows(out)
